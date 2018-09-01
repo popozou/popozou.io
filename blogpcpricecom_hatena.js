65da2d6124cdddc7cc266aa2b83783067d50e0e9
pcpricecom <key@e-bit.jp>
2018-09-02 00:26:42 +0900

    include
    include

---
+++ blogpcpricecom_hatena.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    $(function(){
+        $("#include-pc").load("https://popozou.github.io/popozou.io/include-pc.html");
+    })
+    $(function(){
+        $("#include-sns").load("https://popozou.github.io/popozou.io/include-sns.html");
+    })
+</script>
